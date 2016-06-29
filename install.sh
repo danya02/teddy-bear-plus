168c7254fea77f981a277f75617e5fe20e9d0257
Danya Generalov <danya02@me.com>
2016-06-29 14:40:32 +0000

    Adding dependency.

--- install.sh
+++ install.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-sudo apt install libreadline-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev libjansson-dev libpython-dev make python3-pip -y
-cd /tmp
-git clone --recursive https://github.com/vysheng/tg.git
-cd tg
-./configure --disable-libconfig
-make
-mkdir ~/.teddy_bear_plus
-cp ./bin/telegram-cli ~/.teddy_bear_plus/telegram-cli
-cd /tmp
-git clone --recursive https://github.com/luckydonald/pytg
-sudo pip install pip-utils
-sudo pip install pytg
-sudo pip3 install pip3-utils
-sudo pip3 install pytg
-
+sudo apt install python3-pip python-pip -y
+sudo pip3 install python-telegram-bot
+sudo pip3 install urllib3>=1.10
