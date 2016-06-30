869c517b76ca96613394cf00ed1778be06b74b25
Danya Generalov <danya02@me.com>
2016-06-30 10:09:22 +0000

    Adding new commands.

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 sudo apt install python3-pip python-pip -y
 sudo pip3 install python-telegram-bot
-sudo pip3 install urllib==1.10
+sudo pip3 install -U urllib3
+sudo pip3 install sounddevice pyaudio
+cd /tmp
+git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg
+cd ffmpeg
+./configure
+make
+sudo make install
--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class Button:
 		self._pin_=pin
 		self._gpio.setmode(self._gpio.BOARD)
 		self._gpio.setwarnings(False)
-		self._gpio.setup(self._pin_,self._gpio.IN,self._gpio.PUD_UP)
+		self._gpio.setup(self._pin_,self._gpio.IN,(self._gpio.PUD_UP if not notgnd else self._gpio.PUD_DOWN))
 	def get_state(self):
 		self._gpio.setmode(self._gpio.BOARD)
 		self._gpio.setwarnings(False)
@@ -75,13 +75,16 @@ global l
 global s
 s=6
 l=RGBLED(7,12,11)
-b=Button(3,False)
+global b1
+b1=Button(3,False)
 def status():
 	from time import sleep
 	global s
 	global l
+	global ofsd
+	global hidden
 	while 1:
-		if s==-1: # hidden
+		if s==-1 or hidden: # hidden
 			l.color=[0,0,0]
 		if s==0: # idle, not controlled
 			l.color=[1,0,0]
@@ -111,7 +114,7 @@ def status():
 			sleep(0.25)
 			l.color=[0,0,1]
 			sleep(0.25)
-		if s==6: # obfuscated
+		if s==6 or obfsd: # obfuscated
 			import random
 			l.color=random.choice([[1,0,0],[0,1,0],[0,0,1],[0,0,0]])
 			sleep(random.random())
@@ -132,19 +135,21 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-
-
+global vol
+vol=50
 # Define a few command handlers. These usually take the two arguments bot and
 # update. Error handlers also receive the raised TelegramError object in error.
 def start(bot, update):
 	bot.sendMessage(update.message.chat_id, text='Hi!')
 
-
 def help(bot, update):
 	bot.sendMessage(update.message.chat_id, text='Help!')
 global mode
 mode=False
-
+global hidden
+hidden=False
+global obfsd
+obfsd=False
 def echo(bot, update):
 	global s
 	global mode
@@ -156,13 +161,80 @@ def echo(bot, update):
 			bot.sendMessage(update.message.chat_id, text="Succesfully set mode to "+str(s))
 		except:bot.sendMessage(update.message.chat_id, text="Failed to set mode to "+str(s))
 	mode=False
-		
+def statset(status):
+	global s
+	global hidden
+	if not obfs and hidden:s=status
+	elif obfs:s=6
+	elif hidden:s=-1
 def error(bot, update, error):
 	logger.warn('Update "%s" caused error "%s"' % (update, error))
 def setmode(bot,update):
 	bot.sendMessage(update.message.chat_id, text="Send, as number in -1~7, the displayed mode of operation.")
 	global mode
 	mode=True
+def connect():statset(1)
+def disconnect():statset(0)
+def vol_up():
+	global vol
+	import os
+	vol+=5
+	os.popen("amixer set PCM "+str(vol)+"%").read()
+def vol_down():
+	global vol
+	import os
+	vol-=5
+	os.popen("amixer set PCM "+str(vol)+"%").read()
+def vol_set(v):
+	global vol
+	import os
+	vol=v
+	os.popen("amixer set PCM "+str(vol)+"%").read()
+def hide(bot,update):
+	bot.sendMessage(update.message.chat_id, text="Device status shown.")
+	global hidden
+	hidden=False
+def unhide(bot,update):
+	bot.sendMessage(update.message.chat_id, text="Device status hidden.")
+	global hidden
+	hidden=False
+def obfs(bot,update):
+	bot.sendMessage(update.message.chat_id, text="Device status obfuscated.")
+	global obfsd
+	obfsd=True
+def unobfs(bot,update):
+	bot.sendMessage(update.message.chat_id, text="Device status not obfuscated.")
+	global obfsd
+	obfsd=False
+def getvol(bot,update):
+	global vol
+	bot.sendMessage(update.message.chat_id, text="Current volume: "+str(vol)+"%")
+def upvol(bot,update):
+	global vol
+	vol+=5
+	setvol(vol)
+	bot.sendMessage(update.message.chat_id, text="Current volume: "+str(vol)+"%")
+def downvol(bot,update):
+	global vol
+	vol-=5
+	vol_set(vol)
+	bot.sendMessage(update.message.chat_id, text="Current volume: "+str(vol)+"%")
+#def photo(update,bot):
+#	statset(5)
+#	bot.sendChatAction(update.message.chat_id,telegram.ChatAction.UPLOAD_PHOTO)
+#	import pygame.camera
+#	pygame.camera.init()
+#	cam=pygame.camera.Camera(pygame.camera.list_cameras()[0])
+#	cam.start()
+#	import time
+#	time.sleep(5)
+#	statset(-1)
+#	time.sleep(1)
+#	img=cam.get_image()
+#	statset(5)
+#	import pygame.image
+#	pygame.image.save(img,"/tmp/img.
+#	statset(1)
 def telegrammar():
     # Create the EventHandler and pass it your bot's token.
 	updater = Updater("135232412:AAFfA6JImKl4sxv35IAw2f2Zjq7gb67Jk7Q")
@@ -174,9 +246,30 @@ def telegrammar():
 	dp.add_handler(CommandHandler("start", start))
 	dp.add_handler(CommandHandler("help", help))
 	dp.add_handler(CommandHandler("mode",setmode))
+	dp.add_handler(CommandHandler("hide",hide))
+	dp.add_handler(CommandHandler("unhide",unhide))
+	dp.add_handler(CommandHandler("obfs",obfs))
+	dp.add_handler(CommandHandler("unobfs",obfs))
+	dp.add_handler(CommandHandler("getvol",getvol))
+	dp.add_handler(CommandHandler("upvol",upvol))
+	dp.add_handler(CommandHandler("downvol",downvol))
+#	dp.add_handler(CommandHandler("photo",photo))
     # on noncommand i.e message - echo the message on Telegram
 	dp.add_handler(MessageHandler([Filters.text], echo))
 	dp.add_error_handler(error)
 	updater.start_polling()
 	updater.idle()
-telegrammar()
+def record():
+	import subprocess
+	c1="arecord -D plughw:1,0 -".split(" ")
+	c2="ffmpeg -i pipe: -f wav /tmp/rec.ogg".split(" ")
+	p1=subprocess.Popen(c1,stdout=subprocess.PIPE)
+	p2=subprocess.Popen(c2,stdin=p1.stdout)
+	global b1
+	statset(4)
+	b1.wait_for(b1._gpio.RISING)
+	p1.terminate()
+	p2.communicate()
+	statset(1)
+#telegrammar()
+
